39d040182f327e57c984703713e879c2ffc0d532
Ryan1534 <36740030+Ryan1534@users.noreply.github.com>
2018-04-10 12:29:12 -0400

    RWatkins
    Robotics at AIS

--- QuestionAnswerer.swift
+++ QuestionAnswerer.swift
@@ -1,77 +1,51 @@
-struct MyQuestionAnswerer {
-        func responseTo(question: String) -> String {
+struct QuestionAnswerer {
+    func responseTo(question: String) -> String {
+        let lowerQuestion = question.lowercased()
+        
+        if lowerQuestion.contains("practice") && lowerQuestion.contains("long") || lowerQuestion.contains("preperation") {
+            return "You normally get around 5 months to prepare for the main event. Once you get the main table you can do whatever you want!"
+        } else if lowerQuestion.contains("event") || lowerQuestion.contains("date") {
+            return "Usually the event takes place in December, and the next round in January to February. This year it took place January 14."
+        } else if lowerQuestion.contains("age") || lowerQuestion.contains("grade") {
+            return "FLL is focused for primary schoolers and middles schoolers. If you want to continue this in high school you should join FRC. It’s like FLL on steroids."
+        } else if lowerQuestion.contains("class") || lowerQuestion.contains("ict") {
+            return "During ICT class you have the option to work on your robot then so you get extra class time and more time to expand your learning. "
+        } else if lowerQuestion.contains("rewards") || lowerQuestion.contains("trophies") {
+            return "Like most big events, there are rewards. That doesn’t mean they are easy to get. But the real reward is the experience."
+        } else if lowerQuestion.contains("rounds") || lowerQuestion.contains("championship") {
+            return "There are many rounds. Usually the school must qualify then go up through the ranks such as stat and regional until the reach the world championship."
+        } else if lowerQuestion.contains("meeting") || lowerQuestion.contains("practice") {
+            return "Usually there are meetings in the ELC from 4:00 - 6:00 on Wednesdays and Fridays to work on all the different aspects of the competition."
+        } else if lowerQuestion.contains("theme") || lowerQuestion.contains("topic") && lowerQuestion.contains("year"){
+            return "This year the theme of the robot and project was Hydrodynamics. The 2018 / 2019 hasn’t been announced yet, but it rumored to be space exploration!"
+        } else if lowerQuestion.contains("aspects") || lowerQuestion.contains("project") {
+            return "The whole competition takes place in four categories. Project, core values, robot design robot games. To find out more see: www.firstlegoleague.org"
+        } else if lowerQuestion.contains("software") && lowerQuestion.contains("used") || lowerQuestion.contains("programming") || lowerQuestion.contains("download"){
+            return "The robot is created in legos and used the lego mindstorm hardware. The programming platform is called EV3 and is a drag and drop code style."
+        } else if lowerQuestion.contains("cost") || lowerQuestion.contains("benefits") {
+            return "It is free to join and to participate in the events but you might need to buy some of the materials such as the legos and the game board."
+        } else if lowerQuestion.contains("location") || lowerQuestion.contains("place") {
+            return "Every year the location changes but they are all within a small area and are very efficient for the tournament. Check with your current coach to see. "
+        } else if lowerQuestion.contains("experience") || lowerQuestion.contains("knowledge") {
+            return "Anyone can do FLL. People that are beginners to Robotics or experts fill all find interest in this club. Ask people who participate for more help. "
+        } else if lowerQuestion.contains("resume") || lowerQuestion.contains("credit") {
+            return "Doing this will not be graded but it can help you stay ahead in class which results in better grades. It is also a good learning experience for high school. "
+        } else if lowerQuestion.contains("CAS") || lowerQuestion.contains("curricular") {
+            return "Yes, FLL does count toward your CAS hours. Even though CAS isn’t vital in middle school it does help prepare for high school and is a great experience. "
+        } else if lowerQuestion.contains("commitment") || lowerQuestion.contains("effort") {
+            return "This is a serious task so you must come to every meetup to stay up to speed and learn new aspects of the competition. Failure to comply leads to removal."
+        } else if lowerQuestion.contains("team") || lowerQuestion.contains("social") {
+            return "This event does focus on team activities, because one of the four aspects is about team building. Everyone must know everyone else well, to succeed. "
+        } else {
+            let defaultNumber = question.count % 3
             
-            let lowerQuestion = question.lowercased()
-            
-       
-            //Questions and Answers
-//Q. Who is Ricegum?
-            if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("who is") {
-                return "RiceGum or Rice, is an American YouTuber. He has over 9 million subscribers and over 1 billion views as of January 1, 2018"
-//Q. Who is Ricegum’s best friend?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("best friend") {
-                return "AfroGum. He is a character in RiceGum's videos."
-//Q. What is Ricegums real name?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("real name") {
-                return "Bryan Lee"
-//Q. What is Ricegums favorite color?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("favorite") && lowerQuestion.contains ("color") {
-                return "Ricegum’s favorite color as of February 2018 is red."
-//Q. How did Ricegum start being a youtuber?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("how") && lowerQuestion.contains ("youtuber") {
-                return "He opened both his YouTube and Twitter account in October of 2012,about gaming."
-//Q. How did Ricegum convince his mom to become a youtuber?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("convince") && lowerQuestion.contains ("mom") {
-                return "Ricegum decided to take his mother on a trip to one of his meet ups, to show his mother that he is respected and loved to many different people around the world."
-//Q. When was Ricegum Born?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("born") {
-                return "November 19, 1996 (age 21 as of Feb, 2018)"
-//Q. When did Ricegum move to LA?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("move") && lowerQuestion.contains ("la") {
-               return "Ricegum moved to LA on February 14th, 2015."
-//Q. Where was Ricegum born and raised?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("born") {
-                return "Ricegum of Chinese and Vietnamese descent who was born and raised in Las Vegas, Nevada."
-//Q. Where did Ricegum learn how to act infront of his audience?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("learn") && lowerQuestion.contains ("act") {
-                return "Ricegum started with nothing but a camera, then he learned what the community wants to see as he was very socially active with his audience."
-//Q. Has Ricegum flexed in every single video?
-            } else if lowerQuestion == " has ricegum flexed in every single video?" {
-                return " Ricegum flexes in all his videos, with Supreme, Gucci, Bape, Supreme LV, Givenchy, Yeezys, Saint Laurent, Fear Of God, Versace, and many more…"
-//Q. Has Ricegum spend 4 days without showering this year?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("showering") {
-                return "Ricegum publicly announced in one of his multiple vlog videos in a Q & A in 2018."
-//Q. Can Ricegum lose if he fights Jake Paul?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("fights") && lowerQuestion.contains ("jake paul"){
-                return "It is physically impossible for ricegum to lose against Jake Paul. Facts are facts!"
-//Q. Can Ricegum afford 16 Lamborghinis?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("lamborghinis") {
-                return "Ricegum with his current earnings on youtube per video, can afford the cars, yet it would be quite difficult for him to repeat that purchase after a year."
-//Q. Why is Ricegum successful?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("successful") {
-                return "In 2015, one of his videos named These Kids Must Be Stopped went viral on the internet. That was where he got his mass recognition"
-//Q. Why did Ricegum dropout of college?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("college") {
-                return " Ricegum after graduation high school, decided to dropout of college to focus his time on Youtube, and becoming a social comedian."
-//Q. Which person does Ricegum like the most?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("like")  {
-                return "Ricegum’s mother. Ricegum loves his mother and treats her with all his respect. He frequently purchases his mother presents and gifts."
-//Q. Which person does Ricegum hate the most?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("hate")  {
-                return "Jake Paul, or Logan Paul. At the moment, based on our databases they do seem to be equal."
-//Q. Does Ricegum have a girlfriend?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("girlfriend")  {
-                return " RiceGum has always kept his personal relationship at bay. Fans forever wondered about the details of his girlfriend. Sommer Ray rumored to be dating Ricegum."
-//Q. Does Ricegum love playing fortnite?
-            } else if lowerQuestion.contains("ricegum") && lowerQuestion.contains ("fortnite") {
-                return "Ricegum not the best at the game, yet he does live stream on twitch his gameplay and does have fun playing the game in general."
-
+            if defaultNumber == 0 {
+                return "That really depends"
+            } else if defaultNumber == 1 {
+                return "I think so..."
             } else {
-                return "This question is not included in our database, please try a different question."
+                return "Ask me again tomorrow"
             }
         }
-        
     }
-
-
-
+}
